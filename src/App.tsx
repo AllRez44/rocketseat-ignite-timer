23e56130268576d13ec785d617d83d17579e7cda
Allan Resende <91804688+AllRez44@users.noreply.github.com>
2024-03-23 00:32:33 -0300

    Add rocketseat eslint config lib; update esling config to rocketseat; refactoring linting

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import { ThemeProvider } from 'styled-components';
-import './App.css';
-import { Button } from './components/Button';
-import defaultTheme from './styles/themes/default';
-import { GlobalStyle } from './styles/global';
+import { ThemeProvider } from 'styled-components'
+import './App.css'
+import { Button } from './components/Button'
+import defaultTheme from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1 className="title">
-        Hello World
-      </h1>
+      <h1 className="title">Hello World</h1>
       <Button />
       <Button />
       <Button />
--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,28 +1,27 @@
+import styled, { css } from 'styled-components'
 
-import styled, { css } from 'styled-components';
-
-export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success';
+export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success'
 
 interface ButtonContainerProps {
-    variant?: ButtonVariant;
+  variant?: ButtonVariant
 }
 
 const buttonVariants = {
-    primary: 'purple',
-    secondary: 'orange',
-    danger: 'red',
-    success: 'gress'
+  primary: 'purple',
+  secondary: 'orange',
+  danger: 'red',
+  success: 'gress',
 }
 
 export const ButtonContainer = styled.button<ButtonContainerProps>`
-    width: 120px;
-    height: 40px;
-    border-radius: 4px;
-    border: 0;
-    margin: 8px;
+  width: 120px;
+  height: 40px;
+  border-radius: 4px;
+  border: 0;
+  margin: 8px;
 
-    ${({ theme, variant }) => css`
-        background-color: ${variant ? buttonVariants[variant] : theme['green-500']};
-        color: ${theme.white};
-    `}
-`   
+  ${({ theme, variant }) => css`
+    background-color: ${variant ? buttonVariants[variant] : theme['green-500']};
+    color: ${theme.white};
+  `}
+`
