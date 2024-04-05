e3ab9b87f0afb4c970e39ef7de465a750fd4e1ca
Allan Resende <91804688+AllRez44@users.noreply.github.com>
2024-04-05 05:47:24 -0300

    Styling Header component & DefaultLayout

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -9,11 +9,11 @@ export const GlobalStyle = createGlobalStyle`
 
     :focus {
         outline: 0;
-        box-shadow: 0 0 0 2px ${({ theme }) => theme['gray-500']};
+        box-shadow: 0 0 0 2px ${(props) => props.theme['gray-500']};
     }
 
     body {
-        background-color: ${({ theme }) => theme['gray-900']};
+        background-color: ${(props) => props.theme['gray-900']};
         color: ${({ theme }) => theme['gray-300']};
     }
 
